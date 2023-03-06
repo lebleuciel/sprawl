92b0c244a9dc1c7a5722d5e62897003c2a41e836
lebleuciel <93124243+lebleuciel@users.noreply.github.com>
2023-03-06 23:52:18 +0330

    add userinfo endpoint

--- controllers/getControllers.js
+++ controllers/getControllers.js
@@ -1,11 +1,25 @@
 exports.landing = (req, res) => {
-  res.send('working!!')
   console.log('hello from landing!!')
 }
 
 exports.userInfo = (req, res) => {
-  res.send('working!!')
   console.log('hello from userInfo!!')
+  const connection = require('../app')
+  connection.query(
+    `SELECT * FROM users WHERE id = ${req.body.id} LIMIT 1`,
+    (error, results) => {
+      if (error) {
+        console.error(error)
+      } else {
+        if (results.length > 0) {
+          console.log('info for User with ID ' + req.body.id)
+          console.log(results)
+        } else {
+          console.log('User with ID ' + req.body.id + ' does not exist')
+        }
+      }
+    },
+  )
 }
 
 exports.transactionHistory = (req, res) => {
